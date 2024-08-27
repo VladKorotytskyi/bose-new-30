ffdad68e2620cc9792c981149ee9235b5901ed99
VikaMelnyk58 <153006020+VikaMelnyk58@users.noreply.github.com>
2024-08-27 13:31:12 +0300

    27.08

---
+++ src/faq.js
@@ -0,0 +1,9 @@
+// vika
+const faqItems = document.querySelectorAll('.faq__item');
+faqItems.forEach((item) => {
+  const question = item.querySelector('.faq__question');
+  const answer = item.querySelector('.faq__answer');
+  question.addEventListener('click', () => {
+    item.classList.toggle('open');
+  });
+});
